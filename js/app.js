0bba999af64e3e77c77967e21ab6fd2d464a5db7
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2020-11-07 14:41:40 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,68 +1,59 @@
-// En knapp som återställer allt till originalutseendet
-// Alla ändringar som du har gjort ska ändras tillbaka
-// Byt ut minst ett foto
-// Ändra bakgrundsfärg på minst ett element
-// Ändra text på minst ett element
-// Ändra färg på minst en knapp
-// Ta bort minst 1 element (Måste läggas till igen när man trycker på återställningsknappen)
-// Lägg till en lista var som helst i DOMen (måste tas bort med resetknappen)
-// Listan ska innehålla flera li element
-// Varje li element ska ha en synlig border. (Valfritt utseende)
 
+//-------------------------------- skapa reset change and Knappar -----------------------------------
 
-
-
-
-
-//-------------------------------- skapa resetKnapp-----------------------------------
-
-let skapaResetKnapp = () => {
+let skapaResetChangeKnapp = () => {
     let headerNav = document.getElementById('header-navigation')
     let resetKnapp = document.createElement('button');
     let changeKnapp = document.createElement('button');
     headerNav.appendChild(changeKnapp);
     resetKnapp.innerHTML = ' Reset ';
     changeKnapp.innerHTML = ' Change ';
-
     changeKnapp.style.marginLeft = '10px';
     changeKnapp.style.background = 'blue';
     changeKnapp.style.color = 'white';
     changeKnapp.style.width = '70px';
-
     resetKnapp.style.marginLeft = '10px';
     resetKnapp.style.width = '70px';
     resetKnapp.style.background = 'red';
     resetKnapp.style.color = 'white';
 
+    // ------------------ ResetKnapp addEventListener---------------
 
     resetKnapp.addEventListener('click', () => {
-        headerNav.removeChild(resetKnapp);       /* resetKnapp ta bort sig själv först */
+        headerNav.removeChild(resetKnapp);
         location.reload();
     })
+    // ------------------ ChangeKnapp addEventListener---------------
+
     changeKnapp.addEventListener('click', () => {
         ChangeFunction();
-        // location.reload();
         headerNav.appendChild(resetKnapp);
         headerNav.removeChild(changeKnapp);
     })
 
 }
 
-skapaResetKnapp();
+skapaResetChangeKnapp();
 
 
 
 
+// --------------------------------------------
 
 
-//-----------------------Change Function-----------------------
 let body = document.querySelector('body');
 let newBtnsContainer = document.createElement('div');
 newBtnsContainer.style.display = 'flex';
 newBtnsContainer.style.flexDirection = 'column';
-
 body.insertBefore(newBtnsContainer, body.childNodes[1]);
 let newBtns;
+
+
+//----------------------- Change Function-----------------------
+
+
+
+//  ---------- Creat Change Buttons Function-----------------------
 
 let ChangeFunction = () => {
     let buttonLista = ['Byt ut Fire Sinus Hoodie foto ( Den i mitten )', 'Ändra bakgrundsfärg för Fire Sinus Hoodie texten ( Den i mitten )', 'Ändra text på rubriken och underrubriken för  Fire Sinus Hoodie ( Den i mitten ) ', 'Ändra färg på alla knappar i de tre article ', 'Ta bort Ash Sinus Hoodie article (den första) ', 'Lägg till en lista ( längst ner ) '];
@@ -71,7 +62,6 @@ let ChangeFunction = () => {
         newBtns.className = ("newBtns-Class");
         newBtns.textContent = buttonLista[x];
         newBtnsContainer.appendChild(newBtns);
-        // newBtns.style.background = 'red';
         newBtns.style.color = 'rgb(141, 0, 0)';
         newBtns.style.fontSize = '25px';
         newBtns.style.padding = '20px 0';
@@ -80,61 +70,33 @@ let ChangeFunction = () => {
         console.log(newBtns)
     }
 
-
     aktiveraButtons()
 
-    // -------------------hover- new buttons -----------
-
-    // let hoverClass = document.querySelector('.hover-Class');
-    // hoverClass.style.background='green';
-
-
+    // -------------------addEventListener to change style for the new buttons -----------
 
     newBtnsContainer.addEventListener('mousemove', function (event) {
-        let hColor = event.x ;
-        for (i= 0; i<newBtns.length; i++){
-        switch (event.target) {
-            case newBtns[i]:
-                newBtns[i].style.background = `hsl( ${hColor}, 80%, 70%)`;
-                newBtns[i].style.color = `hsl( 150, 60%, 20%)`;
+        let hColor = event.x;
+        for (i = 0; i < newBtns.length; i++) {
+            switch (event.target) {
+                case newBtns[i]:
+                    newBtns[i].style.background = `hsl( ${hColor}, 80%, 70%)`;
+                    newBtns[i].style.color = `hsl( 150, 60%, 20%)`;
+            }
         }
     }
-
-
-
-        // if (event.target == newBtns[1]) {
-
-        //     newBtns[1].style.background = 'blue';
-        // }
-        // if (event.target == newBtns[i]) {
-        //     newBtns[i].style.background='green'
-        // }
-    }
     )
-
-
     console.log(newBtns[0]);
-    // console.log(newBtnsContainerArr); 
-    // 
-
-
-    //
 }
 
 
-
-
-
-
+// ------------------- fucntion for aktivera change Buttons med addEventListener  -----------
 
 let aktiveraButtons = () => {
-    newBtns = document.querySelectorAll('.newBtns-Class')
-
-
+    newBtns = document.querySelectorAll('.newBtns-Class');
     newBtns[0].addEventListener('click', () => {
         let img = document.querySelector('.art-2 img');
-        img.setAttribute('src', 'img/hoodie-forrest.png')
-        newBtnsContainer.removeChild(newBtns[0])
+        img.setAttribute('src', 'img/hoodie-forrest.png');
+        newBtnsContainer.removeChild(newBtns[0]);
     })
 
 
@@ -167,7 +129,6 @@ let aktiveraButtons = () => {
         mainImg.forEach(x => x.style.width = '60%')
         newBtnsContainer.removeChild(newBtns[4])
 
-
     })
     newBtns[5].addEventListener('click', () => {
         let listArray = ['Kundtjänst', 'Frågor', 'Nyheter', 'om oss']
@@ -181,7 +142,6 @@ let aktiveraButtons = () => {
             listItem.appendChild(listItemtext);
             newLista.appendChild(listItem);
         }
-
         var footer = document.querySelector('footer');
         footer.style.flexDirection = ' column';
         footer.style.alignItems = 'center';
@@ -193,94 +153,5 @@ let aktiveraButtons = () => {
         footer.appendChild(newLista);
         newBtnsContainer.removeChild(newBtns[5])
 
-
     })
-
 }
-    //----- skapa function för att få tillbaka originalutseendet---------
-
-
-    // let originalutseendet = () => {
-    //     img.setAttribute('src', 'img/hoodie-fire.png')
-    //     articleEven.style.background = 'white';
-    //     h2Texten.innerHTML = 'Sinus Hoodie';
-    //     h3Texten.innerHTML = 'Fire';
-    //     articleBtn.forEach(x => x.style.background = '#222');
-    //     // main[0].appendChild(theFirstArticle);
-    //     main[0].insertBefore(theFirstArticle, main[0].childNodes[0]);
-    //     mainImg.forEach(x => x.style.width = '80%')
-    //     footer.removeChild(newLista);
-
-    // }
-    // -----------------------------------------------------------------------------------
-
-
-
-//     //---------- Byt ut minst ett foto --------
-//     let img = document.querySelector('.art-2 img');
-//     img.setAttribute('src', 'img/hoodie-forrest.png')
-//     // -----------------------------------------------------------------------------------
-
-
-
-// }
-
-//     //----------------- Ändra bakgrundsfärg -------------------
-
-//     let articleEven = document.querySelector('main article:nth-child(even)');
-//     articleEven.style.background = 'rgb(106, 148, 96';
-
-
-//     // -------------- Ändra texten ------------------
-
-
-//     let h2Texten = document.querySelector('.art-2 h2');
-//     h2Texten.innerHTML = 'Nice Green Hoodie';
-//     let h3Texten = document.querySelector('.art-2 h3');
-//     h3Texten.innerHTML = 'Green';
-
-
-
-//     //---------------- Ändra färg på knapp--------------
-
-//     let articleBtn = document.querySelectorAll('article button');
-//     articleBtn.forEach(x => x.style.background = 'rgb(156, 126, 62)');
-
-
-//     // ------------------------Ta bort element -----------------
-
-
-//     let main = document.getElementsByTagName('main');
-//     let theFirstArticle = document.querySelector('.art-1');
-//     let mainImg = document.querySelectorAll('main article figure img')
-//     main[0].removeChild(theFirstArticle);
-//     mainImg.forEach(x => x.style.width = '60%')
-
-//     console.log(main);
-//     console.log(theFirstArticle);
-//     console.log(document);
-
-
-//     // ------------- Lägg till en lista med flera li och border  ---------------
-
-//     let listArray = ['Kundtjänst', 'Frågor', 'Nyheter', 'om oss']
-//     var newLista = document.createElement("ul");
-//     for (let i = 0; i < listArray.length; i++) {
-//         var listItem = document.createElement("li");
-//         var listItemtext = document.createTextNode(listArray[i]);
-//         listItem.style.border = 'solid 3px black';
-//         listItem.style.color = 'black';
-//         listItem.style.padding = '3px 30px';
-//         listItem.appendChild(listItemtext);
-//         newLista.appendChild(listItem);
-//     }
-
-//     var footer = document.querySelector('footer');
-//     footer.style.flexDirection = ' column';
-//     footer.style.alignItems = 'center';
-//     newLista.style.display = ' flex';
-//     newLista.style.justifyContent = 'space-between';
-//     newLista.style.width = ' 1000px';
-//     newLista.style.fontWeight = ' bold';
-//     newLista.style.listStyle = 'none';
-//     footer.appendChild(newLista);
